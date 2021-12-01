207ca0e115377d0ae1255997d7ed257893add608
Aman Kumar <kumaramansingh137@gmail.com>
2021-12-01 11:31:22 +0530

    Add files via upload

---
+++ Stacks and Queues/Stack Python/BalancedParanthesis.py
@@ -0,0 +1,45 @@
+
+def isBalanced(string):
+
+	s = []
+
+	for char in string:
+
+		if char in  '({[':
+
+			s.append(char)
+
+		elif char == ')':
+
+			if (not s or s[-1] != '('):
+				return False
+
+			s.pop()
+
+		elif char == '}':
+
+			if (not s or s[-1] != '{'):
+				return False
+
+			s.pop()
+
+		elif char == ']':
+
+			if (not s or s[-1] != '['):
+				return False
+
+			s.pop()
+
+
+	if (not s):
+		return True
+
+	else:
+		return False
+
+
+string = input()
+
+ans = isBalanced(string)
+
+print(ans)
---
+++ Stacks and Queues/Stack Python/StackQueueInBuilt.py
@@ -0,0 +1,38 @@
+
+import queue
+
+
+
+# inbuilt stack as List.
+
+# s = [1, 2, 3]
+# s.append(4)
+# s.append(5)
+
+# print(s.pop())
+# print(s.pop())
+
+
+# inbuilt queue.
+
+# q = queue.Queue()
+
+# q.put(1)
+# q.put(2)
+# q.put(3)
+# q.put(4)
+
+# while not q.empty():
+# 	print(q.get())      # Output will follow  FIFO property (First In First Out)
+
+
+# inbuilt stack as queue.
+
+q = queue.LifoQueue()
+
+q.put(1)
+q.put(2)
+q.put(3)
+
+while not q.empty():
+	print(q.get())      # Output will follow LIFO property (Last In First Out).
